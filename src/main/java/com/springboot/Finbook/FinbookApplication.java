dca060003b413d7a192c3ad1af95a5626ce98c1f
brieey <briannanichole247@gmail.com>
2024-03-21 13:42:01 -0500

    sample commit

---
+++ src/main/java/com/springboot/Finbook/FinbookApplication.java
@@ -0,0 +1,13 @@
+package com.springboot.Finbook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinbookApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FinbookApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/springboot/Finbook/FinbookApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springboot.Finbook;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinbookApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
